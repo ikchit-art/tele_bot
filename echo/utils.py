d88a2d9d8cc6c29dd7fc46d7e8efbf6920df8206
Vladimir <volkvid@yandex.ru>
2019-05-09 01:27:07 +0300

    Добавить возможность отправлять уведомление о курсе валют в виде картинки

--- echo/utils.py
+++ echo/utils.py
@@ -1,9 +1,6 @@
 import random
-import ssl
 import time
 import os
-import urllib.error
-import urllib.request
 from logging import getLogger
 from io import BytesIO
 
@@ -33,3 +30,19 @@ def get_filename():
     filename = "result_{}_{}.png".format(int(time.time()), random.randint(1, 100))
     logger.debug("Сохраняю в файл `%s`", filename)
     return filename
+
+
+def save_image(img: Image, img_format=None, quality=85):
+    """ Сохранить картинку из потока в переменную для дальнейшей отправки по сети
+    """
+    if img_format is None:
+        img_format = img.format
+    output_stream = BytesIO()
+    output_stream.name = 'image.jpeg'
+    # на Ubuntu почему-то нет jpg, но есть jpeg
+    if img.format == 'JPEG':
+        img.save(output_stream, img_format, quality=quality, optimize=True, progressive=True)
+    else:
+        img.convert('RGB').save(output_stream, format=img_format)
+    output_stream.seek(0)
+    return output_stream
